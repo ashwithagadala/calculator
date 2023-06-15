6b8163ec87cc65cec3381039b2e178a2bfa876eb
ashwithagadala <136691474+ashwithagadala@users.noreply.github.com>
2023-06-15 14:16:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//todo:Make m+ and M- and mc functional
+let string="";
+let buttons=document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML== '='){
+            string=eval(string);
+            document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML=='AC'){
+            string=""
+            document.querySelector('input').value=string;
+        }
+        else{
+        console.log(e.target)
+        string=string + e.target.innerHTML;
+        document.querySelector('input').value=string;
+    }
+    })
+})
